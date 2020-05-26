2eb64b9859e02527a6d74ee5f67734174d6b8fcb
Takumi Uchida <mitauchiku@gmail.com>
2020-05-26 22:28:55 +0900

    mod:関数分割

--- server3.c
+++ server3.c
@@ -9,11 +9,19 @@
 #include <stdlib.h>
 
 #define HTTP_TCP_PORT 8080
-#define CR 13
-#define LF 10
 
-void http(int sockfd);
-int send_msg(int fd, char *msg);
+#define ERR_BDY_404 "404 Page not found."
+#define ERR_BDY_405 "405 Method not allowed, Get only."
+#define ERR_BDY_500 "500 Internal Server Error."
+#define RSP_HDR_200 "HTTP/1.1 200 OK\r\ntext/html\r\n\r\n"
+#define RSP_HDR_404 "HTTP/1.1 404 Not Found\r\ntext/html\r\n\r\n"
+#define RSP_HDR_405 "HTTP/1.1 405 Method Not Allowed\r\ntext/html\r\n\r\n"
+#define RSP_HDR_500 "HTTP/1.1 500 Internal Server Error\r\ntext/html\r\n\r\n"
+
+void serv(int sockfd);
+int send_msg(int fd, char *msg, int len);
+int send_error_msg(int fd, int status_code);
+int send_header(int fd, int status_code);
 
 int main()
 {
@@ -27,34 +35,36 @@ int main()
 
   if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
   {
-    fprintf(stderr, "error: socket()\n");
+    perror("Failed to create socket ");
     exit(1);
   }
 
   if (bind(sockfd, (struct sockaddr *)&reader_addr, sizeof(reader_addr)) < 0)
   {
-    fprintf(stderr, "error: bind()\n");
+    perror("Failed to bind connection ");
     close(sockfd);
     exit(1);
   }
 
   if (listen(sockfd, 10) < 0)
   {
-    fprintf(stderr, "error: listen()\n");
+    perror("Failed to starting listen");
     close(sockfd);
     exit(1);
   }
+
+  fprintf(stdout, "serving on port %d\n", HTTP_TCP_PORT);
 
   while (1)
   {
     if ((new_sockfd = accept(sockfd, (struct sockaddr *)&writer_addr, &writer_len)) < 0)
     {
-      fprintf(stderr, "error: accepting a socket.\n");
+      perror("Failed to accept socket connection ");
       break;
     }
     else
     {
-      http(new_sockfd);
+      serv(new_sockfd);
       close(new_sockfd);
     }
   }
@@ -62,71 +72,109 @@ int main()
   close(sockfd);
 }
 
-void http(int sockfd)
+void serv(int sockfd)
 
 {
   int len;
-  int read_fd;
-  char buf[1024];
-  char meth_name[16];
-  char uri_addr[256];
+  FILE *filep;
+  char recv_buf[1024];
+  char send_buf[1024];
+  char method_name[10];
+  char path[256];
   char http_ver[64];
-  char *uri_file;
+  char *file_name;
 
-  if (read(sockfd, buf, 1024) <= 0)
+  if (read(sockfd, recv_buf, 1024) <= 0)
   {
-
-    fprintf(stderr, "error: reading a request.\n");
+    perror("Failed to read a request ");
+    send_error_msg(sockfd, 500);
   }
   else
   {
-    sscanf(buf, "%s %s %s", meth_name, uri_addr, http_ver);
-    if (strcmp(meth_name, "GET") != 0)
+    sscanf(recv_buf, "%s %s %s", method_name, path, http_ver);
+    if (strcmp(method_name, "GET") != 0)
     {
-      send_msg(sockfd, "501 Not Implemented");
+      send_error_msg(sockfd, 405);
     }
     else
     {
-      if (strcmp(uri_addr, "/") == 0)
+      if (strcmp(path, "/") == 0)
       {
-        uri_file = "index.html";
+        file_name = "index.html";
       }
       else
       {
-        uri_file = uri_addr + 1;
+        file_name = path + 1;
       }
-        
-      if ((read_fd = open(uri_file, O_RDONLY, 0666)) == -1)
-      {
-        send_msg(sockfd, "404 Not Found");
-      }
-      else
-      {
-        send_msg(sockfd, "HTTP/1.0 200 OK\r\n");
-        send_msg(sockfd, "text/html\r\n");
-        send_msg(sockfd, "\r\n");
 
-        while ((len = read(read_fd, buf, 1024)) > 0)
-        {
-          if (write(sockfd, buf, len) != len)
-          {
-            fprintf(stderr, "error: writing a response.\n");
-            break;
-          }
-        }
-        close(read_fd);
+      if ((filep = fopen(file_name, "r")) == NULL)
+      {
+        send_error_msg(sockfd, 404);
+      }
+      else
+      {
+        send_header(sockfd, 200);
+        len = fread(send_buf, 1, 1024, filep);
+        send_msg(sockfd, send_buf, len);
+        fclose(filep);
       }
     }
   }
 }
 
-int send_msg(int fd, char *msg)
+int send_msg(int fd, char *msg, int len)
 {
-  int len;
-  len = strlen(msg);
   if (write(fd, msg, len) != len)
   {
-    fprintf(stderr, "error: writing.");
+    perror("Failed to send message");
   }
   return len;
+}
+
+int send_header(int fd, int status_code)
+{
+  switch (status_code)
+  {
+  case 200:
+    return send_msg(fd, RSP_HDR_200, strlen(RSP_HDR_200));
+    break;
+  case 404:
+    return send_msg(fd, RSP_HDR_404, strlen(RSP_HDR_404));
+    break;
+  case 405:
+    return send_msg(fd, RSP_HDR_405, strlen(RSP_HDR_405));
+    break;
+  case 500:
+    return send_msg(fd, RSP_HDR_500, strlen(RSP_HDR_500));
+    break;
+  default:
+    return -1;
+    break;
+  }
+}
+
+int send_error_msg(int fd, int status_code)
+{
+  if (!send_header(fd, status_code))
+  {
+    perror("Undefined error");
+    return -1;
+  }
+
+  switch (status_code)
+  {
+  case 404:
+    return send_msg(fd, ERR_BDY_404, strlen(ERR_BDY_404));
+    break;
+  case 405:
+    return send_msg(fd, ERR_BDY_405, strlen(ERR_BDY_405));
+    break;
+  case 500:
+    return send_msg(fd, ERR_BDY_500, strlen(ERR_BDY_500));
+    break;
+  default:
+    perror("Undefined error");
+    return -1;
+    break;
+  }
 }
